5d077d497818d35a10ff107743b764f18c2c3f8d
Patrick Rock <patrick.rock@ttuhsc.edu>
2016-06-17 16:09:16 -0500

    init

---
+++ makeplot.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rsript 
+
+#first arg is hbond input, second file is graph output name
+library("ggplot2", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
+library(cowplot)
+
+makeplot <- function(data, color, title){
+  hbonds <- read.table(data, quote="\"", comment.char="")
+  plot <- ggplot(hbonds, aes(x=V2)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, alpha = 0.35) +
+  geom_density(alpha=.2, fill=color) +
+  xlim(0, 25) +
+  theme_classic() +
+  ggtitle(title) +
+  labs(x="Hydrogen Bonds", y="Frequency")
+  return(plot)
+}
+
+p1 <- makeplot("hbonds_1.dat", "red", "alpha=100, dual=off")
+p2 <- makeplot("hbonds_2.dat", "blue", "alpha=150, dual=off")
+p3 <- makeplot("hbonds_3.dat", "green", "alpha=200, dual=off")
+p4 <- makeplot("hbonds_4.dat", "yellow", "alpha=150, dual=on")
+p5 <- makeplot("hbonds_5.dat", "purple", "alpha=200, dual=on")
+
+p <- plot_grid(p1, NULL, p2, p4, p3, p5, labels=c('A', 'B', 'C', 'D', 'E', 'F'), ncol = 2)
+
+show(p)
