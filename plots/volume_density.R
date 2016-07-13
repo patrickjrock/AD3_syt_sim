0cc5e0af989243523591e6746cb04e37d9d29193
Patrick Rock <patrick.rock@ttuhsc.edu>
2016-07-13 03:15:27 -0500

    minor plot fix

--- plots/volume_density.R
+++ plots/volume_density.R
@@ -5,5 +5,6 @@ library("ggplot2", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
 library(cowplot)
 
 volume <- read.csv("~/Desktop/AD3_syt_sim/data/alphavol.data", sep="")
-p <- ggplot() + geom_density(data = volume[volume$frame > 50, ], aes(x=volume, color=factor(mutant), fill=factor(mutant)), alpha=.3)  + theme_classic() + facet_grid(. ~ c2)
+p <- ggplot() + geom_density(data = volume[volume$frame > 50, ], aes(x=volume, fill=mutant), alpha=.3)  + theme_classic() + facet_grid(. ~ c2) +
+  ggtitle("Volume density")
 show(p)
